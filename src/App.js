58a52e1d6e0c258b32e3dd923373cbde24569577
Your Name <you@example.com>
2026-05-14 14:13:18 +0530

    move amplify.yml to root

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ function App() {
   return (
     <div>
       <h1>My AWS Amplify App</h1>
-      <p>React App Running Successfully</p>
+
+      <p>
+        API URL:
+        {process.env.REACT_APP_API_URL}
+      </p>
+
+      <p>
+        Environment:
+        {process.env.REACT_APP_ENV}
+      </p>
     </div>
   );
 }
